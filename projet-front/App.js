61fe5612297d6fca441f2d57281c30686f8458c9
sabrina <sabrinizem@gmail.com>
2023-04-25 14:22:22 +0200

    bare de monu front

--- projet-front/App.js
+++ projet-front/App.js
@@ -1,10 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Accueil from './composants/Accueil';
+import Login from './composants/Login';
+
+
+const Tab=createBottomTabNavigator()
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>bienvenue chez moi</Text>
+      <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="accueil" component={Accueil}
+        options={{
+          tabBarIcon:()=><MaterialCommunityIcons name='home' size={30}/>
+        }}/>
+        <Tab.Screen name="connexion" component={Login}
+        options={{
+          tabBarIcon:()=><MaterialCommunityIcons name='login' size={30}/>
+        }}/>
+      </Tab.Navigator>
+      </NavigationContainer>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +33,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff'
   },
 });
