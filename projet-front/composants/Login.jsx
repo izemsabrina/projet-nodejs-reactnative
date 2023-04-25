6f95769ce7bfe858ce4146a911cd902f0c89aadd
sabrina <sabrinizem@gmail.com>
2023-04-25 14:59:15 +0200

    rajouter un formulaire

--- projet-front/composants/Login.jsx
+++ projet-front/composants/Login.jsx
@@ -1,14 +1,28 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native'
+import React , {useState} from 'react'
 
-const Login = () => {
+
+const Login = ({navigation}) => {
+    const [login, setLogin] =useState("")
   return (
     <View>
       <Text>Login</Text>
+      <TextInput placeholder="email" style={styles.forme} value={login} onChangeText={(text) => setLogin(text)}/>
+      <TextInput placeholder='password' style={styles.forme}/>
+      <Button onPress={() => navigation.navigate("creer-compte" , {query : login})} title="voulez-vous créer un compte?" />
     </View>
   )
 }
 
+
 export default Login
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    forme : {
+        borderWidth : 1,
+        padding : 15 ,
+        borderColor : "black",
+        backgroundColor : "white",
+        marginBottom : 15
+    }
+})
